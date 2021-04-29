7b2af2959219b287749f97749cfc7ef81e97e98f
Mateusz Grela <grela.mat@gmail.com>
2021-04-29 15:06:13 +0200

    update

--- src/database/mongodb.ts
+++
@@ -1,8 +0,0 @@
-import mongodb from 'mongodb';
-const MongoClient = mongodb.MongoClient;
-
-MongoClient.connect(
-	'url'
-)
-	.then(result => console.log('MongoDB connected...'))
-	.catch(err => console.log(err));
