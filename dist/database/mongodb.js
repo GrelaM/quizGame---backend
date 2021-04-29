0d6ba94149ccc47663b4338d7670ca0398c734b4
Mateusz Grela <grela.mat@gmail.com>
2021-04-29 11:44:42 +0200

    update

---
+++ dist/database/mongodb.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongodb_1 = __importDefault(require("mongodb"));
+const MongoClient = mongodb_1.default.MongoClient;
+MongoClient.connect('mongodb+srv://quizgameadmin:zFzcAHbtaVv6wwt6@cluster0.s37zq.mongodb.net/quizDatabase?retryWrites=true&w=majority')
+    .then(result => {
+    console.log('Connected...');
+})
+    .catch(err => console.log(err));
