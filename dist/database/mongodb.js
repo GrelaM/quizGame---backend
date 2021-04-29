66a0608b79c3bdb37249e8cb8fba39ababf47e91
Mateusz Grela <grela.mat@gmail.com>
2021-04-29 12:05:32 +0200

    update

--- dist/database/mongodb.js
+++ dist/database/mongodb.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongodb_1 = __importDefault(require("mongodb"));
 const MongoClient = mongodb_1.default.MongoClient;
-MongoClient.connect('mongodb+srv://<USER>:<PASSWORD>@cluster0.s37zq.mongodb.net/quizGameDatabase?retryWrites=true&w=majority')
+MongoClient.connect('mongodb+srv://quizgameadmin:zFzcAHbtaVv6wwt6@cluster0.s37zq.mongodb.net/quizGameDatabase?retryWrites=true&w=majority')
     .then(result => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
